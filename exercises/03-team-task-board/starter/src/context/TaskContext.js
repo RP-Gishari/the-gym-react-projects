805840cf7837ed7d5f8c63b8723fe0433375eb47
Remy <shingiroremy303@gmail.com>
2026-04-09 16:05:43 +0200

    feat: return jsx context wrapper

--- exercises/03-team-task-board/starter/src/context/TaskContext.js
+++ exercises/03-team-task-board/starter/src/context/TaskContext.js
@@ -1,4 +1,18 @@
 import { useReducer } from "react";
 import { createContext } from "react";
+import {taskReducer} from "./reducer/taskReducer.js"
+import {initialState} from "./reducer/taskReducer.js"
 
 const TaskContext = createContext();
+
+export default function TaskProvider({children}) {
+    const [state, dispatch] = useReducer(taskReducer,initialState );
+    return(
+        <>
+        <TaskContext.Provider value={{state, dispatch}}>
+        {children}
+         </TaskContext.Provider>
+        </>
+    )
+    
+}
