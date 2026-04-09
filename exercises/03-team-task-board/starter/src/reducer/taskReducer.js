3795eea3013c04eaa11de490b3a114275a5a7371
Remy <shingiroremy303@gmail.com>
2026-04-09 15:18:41 +0200

    feat: complete reducer for payload

---
+++ exercises/03-team-task-board/starter/src/reducer/taskReducer.js
@@ -0,0 +1,76 @@
+import { useReducer } from "react"
+/*
+ACTIONS:
+
+ADD_TASK → Adds a new task to the list
+DELETE_TASK → Removes a task by id
+MOVE_TASK → Updates the status of a task
+SET_FILTER → Updates filtering criteria (assignee + priority)
+
+Why:
+To centralize all task-related state changes and keep logic predictable.
+*/
+
+
+const initialState = {
+    tasks: [],
+    filters: {
+        assigneeId: null,
+        priority: null
+    }
+}
+
+
+function taskReducer(state, action) {
+    switch(action.type) {
+        case 'ADD_TASK': {
+            const {title, priority, assigneeId} = action.payload;
+            const newTask = {
+                id: Date.now(),
+                title,
+                priority,
+                assigneeId,
+                status: 'todo'
+            }
+
+            return {
+                ...state,
+                tasks: [...state.tasks, newTask]
+            }
+        }
+
+        case 'DELETE_TASK': {
+            const {id} = action.payload;
+            const updatedTasks = state.tasks.filter( task => task.id !== id)
+            return{
+                ...state,
+                tasks: updatedTasks
+            }
+        }
+
+        case 'MOVE_TASK': {
+            const {id, newStatus} = action.payload;
+            const updatedTasks = state.tasks.map(task => {
+                if(task.id === id) return{...task, status: newStatus}
+                else return task
+            })
+            return{
+                ...state,
+                tasks: updatedTasks
+            }
+        }
+        
+        case 'SET_FILTER': {
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    ...action.payload
+                }
+            }
+        }
+        
+        default:
+            return state
+    }
+}
