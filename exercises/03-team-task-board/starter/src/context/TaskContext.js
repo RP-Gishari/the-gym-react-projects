4015b540d6dab1fae4044fd2524d32aff0caced8
Remy <shingiroremy303@gmail.com>
2026-04-09 16:20:20 +0200

    feat: final usecontext

--- exercises/03-team-task-board/starter/src/context/TaskContext.js
+++ exercises/03-team-task-board/starter/src/context/TaskContext.js
@@ -2,17 +2,25 @@ import { useReducer } from "react";
 import { createContext } from "react";
 import {taskReducer} from "./reducer/taskReducer.js"
 import {initialState} from "./reducer/taskReducer.js"
+import { useContext } from "react";
 
-const TaskContext = createContext();
+export const TaskContext = createContext();
 
 export default function TaskProvider({children}) {
     const [state, dispatch] = useReducer(taskReducer,initialState );
     return(
-        <>
         <TaskContext.Provider value={{state, dispatch}}>
         {children}
          </TaskContext.Provider>
-        </>
     )
     
+}
+
+export  function useTasks() {
+    const context = useContext(TaskContext);
+    if(!context){
+        throw new Error('useTasks must be used within TaskProvider')
+    }
+
+    return context;
 }
