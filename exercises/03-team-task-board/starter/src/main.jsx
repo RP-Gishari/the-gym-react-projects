12a58dd0cc16c75723b9b2edee80e0adb56efaef
Blandus1 <blandine0045@gmail.com>
2026-05-06 17:29:16 +0200

    added react-router-dom and modified files

--- exercises/03-team-task-board/starter/src/main.jsx
+++ exercises/03-team-task-board/starter/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createContext } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { TaskProvider } from './context/taskContext.jsx';
@@ -9,12 +10,14 @@ import Reducer from './reducer/Reducer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <BrowserRouter>
     <TaskProvider>
-
-     <App />
-
+    <Routes>
+      <Route path="/" element={<App/>}/>
+       {/* <Route path="/:id" element = {</>}/>*/}
+    </Routes>
     </TaskProvider>
+    </BrowserRouter>
    
   </StrictMode>,
 )
