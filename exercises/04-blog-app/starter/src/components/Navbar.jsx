776b3839e496a157ba5e04264c4c9bc3f1ecadd3
Remy <shingiroremy303@gmail.com>
2026-05-09 15:31:38 +0200

    chore: added react router dom

--- exercises/04-blog-app/starter/src/components/Navbar.jsx
+++ exercises/04-blog-app/starter/src/components/Navbar.jsx
@@ -7,14 +7,12 @@ export default function Navbar(){
 
     return(
         <>
-            <nav className="h-20 flex items-center p-1.5">
-                <h1>Blogify</h1>
+            <nav className="h-20 flex items-center p-2.5">
+                <h1 className="font-bold">Blogify</h1>
                 <div className="pl-190">
                     <button>Posts</button>
                     <button>Authors</button>
                 </div>
-
-                <Button/>
             </nav>
         </>
     )
