3efdb9d3def243ae910fd6f929e3f118b794ebc0
Remy <shingiroremy303@gmail.com>
2026-05-09 16:21:25 +0200

    fix: navigation with link

--- exercises/04-blog-app/starter/src/components/Navbar.jsx
+++ exercises/04-blog-app/starter/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Input } from "./ui";
-
+// import { Button, Input } from "./ui";
+ import { NavLink } from "react-router-dom";
 
 export default function Navbar(){
 
@@ -10,8 +10,9 @@ export default function Navbar(){
             <nav className="h-20 flex items-center p-2.5">
                 <h1 className="font-bold">Blogify</h1>
                 <div className="pl-190">
-                    <button>Posts</button>
+                    <button>Psts</button>
                     <button>Authors</button>
+                    <NavLink to='/about'>Linked</NavLink>
                 </div>
             </nav>
         </>
