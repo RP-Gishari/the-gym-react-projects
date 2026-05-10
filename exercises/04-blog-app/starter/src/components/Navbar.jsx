0f5efcc9170225a5d5e6e5058c60adc7aa93027f
Remy <shingiroremy303@gmail.com>
2026-05-10 09:02:23 +0200

    fix: navigation buttons

--- exercises/04-blog-app/starter/src/components/Navbar.jsx
+++ exercises/04-blog-app/starter/src/components/Navbar.jsx
@@ -2,17 +2,24 @@ import React from "react";
 // import { Button, Input } from "./ui";
  import { NavLink } from "react-router-dom";
  import { Search, Bookmark } from "lucide-react";
+ import { Button } from "./ui";
 
 export default function Navbar(){
 
 
     return(
         <>
-            <nav className="flex items-center justify-around pl-1 w-auto h-auto pt-3">
-                <NavLink to='/' className='font-bold text-2xl'>Blogify</NavLink>
+            <nav className="flex justify-around pl-1 w-auto h-auto pt-3 gap-70">
+                <div className="items-start">
+                        <NavLink to='/' className='font-bold text-2xl'>Blogify</NavLink>
+                </div>
                 <div className="flex gap-4 text-lg text-neutral-950">
-                    <NavLink to='/posts'>Posts</NavLink>
-                    <NavLink to='/authors'>Authors</NavLink>
+                    <Button>
+                        <NavLink to='/posts'>Posts</NavLink>
+                    </Button>
+                    <Button>
+                        <NavLink to='/authors'>Authors</NavLink>
+                    </Button>
                     <Search/>
                     <Bookmark/><span>0</span>
 
