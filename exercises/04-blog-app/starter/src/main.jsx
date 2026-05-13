ee7a4d37934172033e73b9f1fc56a6691f8ef513
Blandus1 <blandine0045@gmail.com>
2026-05-13 15:38:52 +0200

    feat: Added the needed route paths

--- exercises/04-blog-app/starter/src/main.jsx
+++ exercises/04-blog-app/starter/src/main.jsx
@@ -10,8 +10,15 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<MainLayout/>}>
-         {/* <Route index element={<App/>}/> */}
+         {/* <Route index element={<Home/>}/>
+         <Route path="posts" element={<PostsList/>}>
+         <Route path=":slug" element={<PostDetail/>}/>
+         </Route>
+         <Route path="categories/:slug" element={<PostCategory/>}/>
+         <Route path="authors/:id" element={<AuthorProfile/>}/> */}
+
       </Route>
+     
      
     </Routes>
     </BrowserRouter>
