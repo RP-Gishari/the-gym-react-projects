22d742c1b9069e1745afb46617157271628ab529
Remy <shingiroremy303@gmail.com>
2026-05-15 00:02:22 +0200

    feat: fetched all endpoints and merge relational data

--- exercises/04-blog-app/starter/src/pages/HomePage.jsx
+++ exercises/04-blog-app/starter/src/pages/HomePage.jsx
@@ -3,27 +3,41 @@ import useApi from '../hooks/useApi'
 import {PostCard} from '../components/PostCard'
 
 export   function HomePage(){
-        const {data, error, loading } = useApi('http://localhost:3001/posts')
+        const {data: posts, error: postsError, loading: postsLoading } = useApi('http://localhost:3001/posts')
+        const {data: users, error: usersError, loading: usersLoading } = useApi('http://localhost:3001/users')
+        const {data: categories, error: categoriesError, loading: categoriesLoading } = useApi('http://localhost:3001/categories')
 
-        if(loading){
-            return(<h2>loading....</h2>)
+        if(postsLoading || usersLoading || categoriesLoading){
+            return <h2>loading....</h2>
         } 
-        if(error){
+        if(postsError || usersError || categoriesError ){
             return <h2>Error occured</h2>
         } 
-        if(!data){
+        if(!posts || !users || !categories){
             return <h2>No dat found</h2>
         }
+
+    const globalPost = posts.map(post =>{
+        
+        const author = users.find(user => post.authorId === user.id)
+        const category = categories.find(category => post.categoryId === category.id)
+
+        return{
+            ...post,
+            author,
+            category
+        }
+    })
      
 
 
 
-     const published = data.filter(post=>{
+     const published = globalPost.filter(post=>{
         return post.status === 'published'
      })
 
      const latest = published.sort((a,b)=> new Date(b.date) - new Date(a.date))
-     const topPosts = latest.slice(0,5)
+     const topPosts = latest.slice(0,6)
 
 
 
