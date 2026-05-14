0a735215cb66a5f8b52b5ca990aaeadd63896bb3
Blandus1 <blandine0045@gmail.com>
2026-05-14 16:44:09 +0200

    fix:removed unnecessary heading

--- exercises/04-blog-app/starter/src/pages/Home.jsx
+++ exercises/04-blog-app/starter/src/pages/Home.jsx
@@ -30,7 +30,6 @@ export default function Home(){
         <>
         {/*Hero section */}
         <section>
-            <h1>Most recent posts goes here</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                {allposts.map(p=>{
                 return <PostCard key={p.id} post={p}/>
