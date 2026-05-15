2698ab9a819bd264517a9c727ad54f9519c3ee89
Remy <shingiroremy303@gmail.com>
2026-05-16 01:35:06 +0200

    feat: filter only published posts

--- exercises/04-blog-app/starter/src/pages/PostsPage.jsx
+++ exercises/04-blog-app/starter/src/pages/PostsPage.jsx
@@ -7,8 +7,22 @@ export   function PostsPage(){
     const {data: categories, error: categoriesError, loading: categoriesLoading } = useApi('http://localhost:3001/categories')
 
     if(postsLoading || usersLoading || categoriesLoading) return <h1>Loading....</h1>
-    if(postsError || usersError || categoriesError) return <h1>an error occu</h1>
+    if(postsError || usersError || categoriesError) return <h1>an error occured</h1>
     if(!posts || !users || ! categories) return <h1>No data found</h1>
+ 
+    const globalPost = posts.map(post =>{
+        const author = users.find(user=>post.authorId === user.id)
+        const category = categories.find(category=> post.categoryId === category.id)
+        return{
+            ...post,
+            author,
+            category
+        }
+    })
+
+    const publishedPosts =  globalPost.filter(post =>post.status === 'published')
+    
+
     return(
         <h2>PostsPage page goes in this file</h2>
     )
