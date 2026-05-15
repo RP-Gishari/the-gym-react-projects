c3867c85488b921f269c7ab098da1954463b9746
Remy <shingiroremy303@gmail.com>
2026-05-16 01:14:58 +0200

    feat: fetched and handling errors in posts page

--- exercises/04-blog-app/starter/src/pages/PostsPage.jsx
+++ exercises/04-blog-app/starter/src/pages/PostsPage.jsx
@@ -2,8 +2,13 @@ import react from "react";
 import useApi from '../hooks/useApi'
 
 export   function PostsPage(){
-    const {data, loading} = useApi('http://localhost:3001/posts')
+    const {data: posts, error: postsError, loading: postsLoading } = useApi('http://localhost:3001/posts')
+    const {data: users, error: usersError, loading: usersLoading } = useApi('http://localhost:3001/users')
+    const {data: categories, error: categoriesError, loading: categoriesLoading } = useApi('http://localhost:3001/categories')
 
+    if(postsLoading || usersLoading || categoriesLoading) return <h1>Loading....</h1>
+    if(postsError || usersError || categoriesError) return <h1>an error occu</h1>
+    if(!posts || !users || ! categories) return <h1>No data found</h1>
     return(
         <h2>PostsPage page goes in this file</h2>
     )
