59a3f65cd4525d9649399e52d4f9854080d866a6
Remy <shingiroremy303@gmail.com>
2026-05-16 00:51:32 +0200

    feat: consumed ui components into post card

--- exercises/04-blog-app/starter/src/components/PostCard.jsx
+++ exercises/04-blog-app/starter/src/components/PostCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card} from './ui/Card'
+import {Card, Badge, Avatar} from './ui'
 import {Link} from 'react-router-dom'
 
 export function PostCard({post}){
@@ -11,19 +11,25 @@ export function PostCard({post}){
                 <Link to={`/posts/${post.slug}`}>
                     <img src={post.coverImage} alt={`cover image for ${post.title}`}></img>
                 </Link>
-                <button>{post.category.name}</button>
-                <Link to={`/posts/${post.title}`}>
+                <Badge>
+                    <Link to={`/categories/${post.category.slug}`}>
+                    <h1>{post.category.name}</h1>
+                </Link> 
+                </Badge>
+                <Link to={`/posts/${post.slug}`}>
                     <h1>{post.title}</h1>
                 </Link>
                 <h2>{post.excerpt}</h2>
                 
-                    <div>
-                        <div>
-                            <img src={post.author.avatar} alt={`author avatar for ${post.author.name}`}></img>
+                    <div className="flex gap-7 items-center">
+                        <div className="flex gap-1 items-center">
+                            <Avatar>
+                                <img src={post.author.avatar} alt={`author avatar for ${post.author.name}`}></img>
+                            </Avatar>
                         <h3>{post.author.name}</h3>
                         </div>
-                        <div>
-                            <h3>{post.readTime}</h3>
+                        <div className="flex">
+                            <span>🕒</span><h3>{post.readTime}</h3>
                         </div>
                     </div>
                 </section>
