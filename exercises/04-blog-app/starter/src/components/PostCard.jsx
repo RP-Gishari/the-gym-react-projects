9cf6f2277137bcf80e2101144c851ec94f6136a3
Remy <shingiroremy303@gmail.com>
2026-05-15 23:10:41 +0200

    fix: passed user and categories data to postcard

--- exercises/04-blog-app/starter/src/components/PostCard.jsx
+++ exercises/04-blog-app/starter/src/components/PostCard.jsx
@@ -6,13 +6,13 @@ export function PostCard({post}){
         <main>
             <section>
                 <img src={post.coverImage}></img>
-                <button>{post.categoryId}</button>
+                <button>{post.category.name}</button>
                 <h1>{post.title}</h1>
                 <h2>{post.excerpt}</h2>
                 
                 <div>
-                    <img src={post.coverImage}></img>
-                    <h3>{post.authorId}</h3>
+                    <img src={post.author.avatar}></img>
+                    <h3>{post.author.name}</h3>
                 </div>
             </section>
             <Card/>
