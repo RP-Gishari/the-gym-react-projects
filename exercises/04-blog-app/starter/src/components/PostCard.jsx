10652442aeebd1920e3fe6ab383f4ef7fa6f40cb
Remy <shingiroremy303@gmail.com>
2026-05-15 23:55:18 +0200

    feat: dynamic routes to coverimage and post title

--- exercises/04-blog-app/starter/src/components/PostCard.jsx
+++ exercises/04-blog-app/starter/src/components/PostCard.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import {Card} from './ui/Card'
+import {Link} from 'react-router-dom'
 
 export function PostCard({post}){
     return(
-        <main>
-            <section>
-                <img src={post.coverImage}></img>
+        <>
+            <Card>
+                
+                <section>
+                <Link to={`/posts/${post.slug}`}>
+                    <img src={post.coverImage} alt="cover-page"></img>
+                </Link>
                 <button>{post.category.name}</button>
-                <h1>{post.title}</h1>
+                <Link to={`/posts/${post.title}`}>
+                    <h1>{post.title}</h1>
+                </Link>
                 <h2>{post.excerpt}</h2>
                 
-                <div>
-                    <img src={post.author.avatar}></img>
-                    <h3>{post.author.name}</h3>
-                </div>
-            </section>
-            <Card/>
-        </main>
+                    <div>
+                        <img src={post.author.avatar} alt="author avatar"></img>
+                        <h3>{post.author.name}</h3>
+                    </div>
+                </section>
+                
+            </Card>
+        </>
 
     )
 }
