f2243816ff52532b3b3ef229cd620015b18ee489
Remy <shingiroremy303@gmail.com>
2026-05-16 10:18:44 +0200

    feat: paragraph spliting and back button

--- exercises/04-blog-app/starter/src/pages/PostDetailPage.jsx
+++ exercises/04-blog-app/starter/src/pages/PostDetailPage.jsx
@@ -1,8 +1,9 @@
 import react from "react";
 import { useParams } from "react-router-dom";
-import {useApi} from '../hooks/useApi'
+import useApi from '../hooks/useApi'
 import { Badge } from "../components/ui/Badge";
 import {Avatar} from '../components/ui/Avatar'
+import {Button} from '../components/ui/Button'
 import { Link } from "react-router-dom";
 
 export   function PostDetailPage(){
@@ -33,9 +34,46 @@ export   function PostDetailPage(){
     })
     if(!singlePost) return <h1>Post not found</h1>
 
+    const paragraph = singlePost.content.split('\n\n')
     
     return(
-        <>
-        </>
+       <main className="max-w-180">
+        <img src={singlePost.coverImage}/>
+        <Link to={`/categories/${singlePost.category.slug}`}>
+            <Badge>
+                {singlePost.category.name}
+            </Badge>
+        </Link>
+        <article>
+            <h1>{singlePost.title}</h1>
+            <div className="flex gap-50">
+                <div className="flex items-center gap-2">
+                    <Avatar
+                    src={singlePost.author.avatar}/>
+                    <Link to={`/authors/${singlePost.author.id}`}>
+                        <h3>{singlePost.author.name}</h3>
+                    </Link>
+                </div>
+                <div className="flex items-center gap-3">
+                    <h3>{singlePost.publishedAt}</h3>
+                    <span>🕒</span><h3>{singlePost.readTime}</h3>
+                </div>
+            </div>
+
+            <section>
+                {paragraph.map(para=>{
+                    return <p key={para.para}>{para}</p>
+                })}
+            </section>
+
+            <Link to='/posts'>
+                <Button>
+                Back to posts
+                </Button>
+            </Link>
+            
+
+        </article>
+       </main>
     )
 }
