cc25c6c04bd9ccb040f3b69e359dc077b6283798
Remy <shingiroremy303@gmail.com>
2026-05-16 08:58:03 +0200

    feat: filtered both search term and selected category

--- exercises/04-blog-app/starter/src/pages/PostsPage.jsx
+++ exercises/04-blog-app/starter/src/pages/PostsPage.jsx
@@ -1,10 +1,14 @@
-import react from "react";
+import react, { useState } from "react";
 import useApi from '../hooks/useApi'
+import {Input} from '../components/ui/Input'
+import {PostCard} from '../components/PostCard'
 
 export   function PostsPage(){
     const {data: posts, error: postsError, loading: postsLoading } = useApi('http://localhost:3001/posts')
     const {data: users, error: usersError, loading: usersLoading } = useApi('http://localhost:3001/users')
     const {data: categories, error: categoriesError, loading: categoriesLoading } = useApi('http://localhost:3001/categories')
+     const [search, setSearch] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState('all')
 
     if(postsLoading || usersLoading || categoriesLoading) return <h1>Loading....</h1>
     if(postsError || usersError || categoriesError) return <h1>an error occured</h1>
@@ -21,9 +25,32 @@ export   function PostsPage(){
     })
 
     const publishedPosts =  globalPost.filter(post =>post.status === 'published')
-    
 
+    const searchFilteredPosts = publishedPosts.filter(item=>{
+        const searched=  item.title.toLowerCase().includes(search.toLowerCase())
+        const selected = selectedCategory === 'all' || item.category.slug === selectedCategory
+        return searched && selected
+    })
     return(
+        <>
         <h2>PostsPage page goes in this file</h2>
+        <Input 
+        value={search}
+        onChange={((e)=>setSearch(e.target.value))}
+        ></Input>
+
+        <button onClick={()=> setSelectedCategory('all')}>All</button>
+        {categories.map(category=>(
+            <button
+            key={category.id}
+            onClick={()=>setSelectedCategory(category.slug)}
+            >{category.name}</button>
+        ))}
+
+            {searchFilteredPosts.map(post=>(
+                <PostCard key={post.id} post={post}/>
+            ))}
+        
+        </>
     )
 }
