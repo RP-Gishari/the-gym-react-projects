63a1c008b46b843f60a243b489a3dd2997205eec
Remy <shingiroremy303@gmail.com>
2026-05-17 20:48:15 +0200

    feat: rendered author  posts in post card

--- exercises/04-blog-app/starter/src/pages/AuthorPage.jsx
+++ exercises/04-blog-app/starter/src/pages/AuthorPage.jsx
@@ -2,6 +2,7 @@ import react from "react";
 import { useParams } from "react-router-dom";
 import useApi from '../hooks/useApi'
 import { Avatar } from "../components/ui/Avatar";
+import {PostCard} from '../components/PostCard'
 
 
 
@@ -10,22 +11,52 @@ export  function AuthorPage(){
     const id = params.id;
 
     const {data: users, error: usersError, loading: usersLoading } = useApi('http://localhost:3001/users')
+    const {data: posts, error: postsError, loading: postsLoading } = useApi('http://localhost:3001/posts')
+    const {data: categories, error: categoriesError, loading: categoriesLoading } = useApi('http://localhost:3001/categories')
 
-    if(usersLoading) return <h1>User loading ....</h1>
-    if(!users) return <h1>No user found</h1>
-    if(usersError) return <h1>an error occured</h1>
+
+    if(postsLoading || usersLoading || categoriesLoading) return <h1>Loading....</h1>
+    if(postsError || usersError || categoriesError) return <h1>an error occured</h1>
+    if(!posts || !users || ! categories) return <h1>No data found</h1>
+
+    const globalPost = posts.map(post =>{
+        const author = users.find(user=> post.authorId === user.id)
+        const category = categories.find(category => post.categoryId === category.id)
+        return{
+            ...post,
+            author,
+            category
+        }
+    })
 
     const currentAuthor = users.find(user=> user.id === id)
     if(!currentAuthor) return <h1>author not found</h1>
 
+    const authorPosts = globalPost.filter(post=>{
+        const published = post.status === 'published'
+        const matchingAuthor = post.authorId === currentAuthor.id
+
+        return published && matchingAuthor
+    })
+
 
     return(
         <section>
-            
+            <h1>Posts by {currentAuthor.name}</h1>
              <Avatar
              src={currentAuthor.avatar}
              size="xl"
              />
+             <h1
+             className="font-bold text-xl">{currentAuthor.name}
+             </h1>
+             <p
+             className="font-bold text-xl">{currentAuthor.bio}
+             </p>
+
+             {authorPosts.map(post => {
+                return <PostCard post={post}/>
+             })}
         </section>
     )
 }
