894d0e86a1b306c2b8c65d108e9ad59f03e795a8
Remy <shingiroremy303@gmail.com>
2026-05-17 19:53:20 +0200

    feat: extracted id from dynamic url

--- exercises/04-blog-app/starter/src/pages/AuthorPage.jsx
+++ exercises/04-blog-app/starter/src/pages/AuthorPage.jsx
@@ -1,6 +1,15 @@
 import react from "react";
+import { useParams } from "react-router-dom";
+import useApi from '../hooks/useApi'
+
+
 
 export  function AuthorPage(){
+    let params = useParams()
+    const id = params.id;
+
+    const {data: users, error: usersError, loading: usersLoading } = useApi('http://localhost:3001/users')
+
 
     return(
         <h2>author page goes in this file</h2>
