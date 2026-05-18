7921acaf91beed015411851ad1f1ac3b1da58de9
Blandus1 <blandine0045@gmail.com>
2026-05-18 21:23:54 +0200

    feat: Added states,input primitive and used useMemo hook

--- exercises/04-blog-app/starter/src/pages/PostList.jsx
+++ exercises/04-blog-app/starter/src/pages/PostList.jsx
@@ -1,9 +1,33 @@
-import { Input } from "../components/ui"
+import { Card, Button, Badge,Avatar,Input } from "../components/ui"
+import {Link, useLoaderData} from "react-router-dom"
+import { apiFetch } from "../hooks/useApi"
+import { useState,useMemo } from "react"
+
+export async function PostListLoader(){
+
+    const [posts, categories]= await apiFetch('/posts', '/categories')
+    const allposts=posts.filter(p=> p.status==='published')
+    return{posts:allposts, categories}
+}
 
 export default function PostList(){
+const {posts,categories}= useLoaderData()
+const [search, setSearch]= useState('')
+const [activeCategory, setActiveCategory]= useState('All')
+
+const filteredPosts= useMemo(()=>{
+    return posts.filter(post=>{
+        const matchedSearch= post.title.toLowerCase().includes(search.toLowerCase())
+        const matchedCategories= activeCategory=== 'All' || post.category=== activeCategory
+        return matchedSearch && matchedCategories
+    })
+},[posts,search,activeCategory])
+
     return (
         <>
-        <Input id label error></Input>
+        <div className="max-w-5xl mx-auto px-4 py-10 flex flex-col gap-8">
+            <Input id="post-search" placeholder="Search posts..." value={search} onchange={e=>setSearch(e.target.value)} />
+        </div>
         </>
     )
 }
