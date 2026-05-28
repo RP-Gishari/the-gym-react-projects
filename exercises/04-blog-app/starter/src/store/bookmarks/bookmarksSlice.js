8ea3148f336f22a6054b8d94258ac60923c27c45
Blandus1 <blandine0045@gmail.com>
2026-05-28 17:53:46 +0200

    feat: Added localStorage to keep bookmarks on refresh

--- exercises/04-blog-app/starter/src/store/bookmarks/bookmarksSlice.js
+++ exercises/04-blog-app/starter/src/store/bookmarks/bookmarksSlice.js
@@ -2,7 +2,8 @@
 //from one object. You write one thing, you get both.
 
 import { createSlice } from "@reduxjs/toolkit";
-import Bookmark from "../../components/Bookmark";
+
+{/*import Bookmark from "../../components/Bookmark";
 
 export const userSlice= createSlice({
     name: "user",
@@ -18,3 +19,19 @@ export const userSlice= createSlice({
 export const {Bookmark}=userSlice.actions
 
 export default userSlice.reducer;
+*/}
+
+//localStorage to help bookmark survive the refresh
+
+function loadFromStorage(){
+    try{
+        const raw= localStorage.getItem('bookmarks')
+        //parse to array since local storage stores only strings
+        return raw? JSON.parse(raw) : []
+    }catch{
+        //refresh if parsing fails
+        return []
+    }
+}
+
+
