78ac3602e780369ec8ba42db0e751cec769116e1
Blandus1 <blandine0045@gmail.com>
2026-06-02 16:17:28 +0200

    feat: Added bookmarksReducer inside the store function

--- exercises/04-blog-app/starter/src/store/store.js
+++ exercises/04-blog-app/starter/src/store/store.js
@@ -3,12 +3,14 @@
 //--reducers show how the store updates itself when it receives instruction(actions=instructions)
 
 //NO CPT PASSES BOOKMARK DATA TO ANOTHER THROUGH PROPS, EVERY CPT CAN TALK TO STORE
+//This means that whatever in store can be accessed by any cpt as long as that cpt dispatches an action.
 import {configureStore} from '@reduxjs/toolkit'
 import bookmarksReducer from './bookmarks/bookmarksSlice'
+
 
 export const store = configureStore({
     reducer:{
         //the slices go here 
-        user: userReducer
+        bookmarks: bookmarksReducer
     }
 })
