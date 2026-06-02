f9d8fc865ddbf72719faa97e097bb55799b15247
Blandus1 <blandine0045@gmail.com>
2026-06-02 16:49:09 +0200

    feat: Added why a comment that explains why adding Provider is essential

--- exercises/04-blog-app/starter/src/main.jsx
+++ exercises/04-blog-app/starter/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-
+import {Provider} from 'react-redux'
+//this provider is the one that helps each cpt to access the store
+import { store } from './store/store'
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App/>    
+    <Provider store={store}>
+    <App/>   
+    </Provider> 
    </StrictMode>,
 )
