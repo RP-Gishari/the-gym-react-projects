99509efaaff44d94252f09326052ef3c52b3beed
Blandus1 <blandine0045@gmail.com>
2026-06-05 16:45:07 +0200

    feat : Added the bookmark button

---
+++ exercises/04-blog-app/starter/src/components/Bookmark.jsx
@@ -0,0 +1,37 @@
+
+import { useSelector,useDispatch } from "react-redux";
+import Bookmark from 'lucide-react'
+import {Button} from './ui'
+//useSelector is how a component reads from the notebook(store)->re-renders when value changes.
+//useDispatch is how a component sends an instruction to the notebook
+import { addBookmark,removeBookmark } from "../store/bookmarks/bookmarksSlice";
+
+
+export default function Bookmark({postId}){
+    const dispatch = useDispatch()
+    const isBookmarked = useSelector(selectIsBookmarked(postId))
+
+    function handleClick(e){
+        e.preventDefault()
+        e.stoPropagation()
+
+        if (isBookmarked){
+            dispatch(removeBookmark(postId))
+        }else {
+            dispatch(addBookmark(postId))
+        }
+    }
+
+
+    return(
+        <>
+        <Button 
+        variant = {isBookmarked? 'primary' : 'secondary'}
+        onClick={handleClick}
+        >
+         <Bookmark className="w-4-h-4"/>
+        
+        </Button>
+        </>
+    )
+}
